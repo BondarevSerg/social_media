db8375c653df4b5252d3e04fed1f23516410cc92
Sergey Bondarev <bondarevsy163@gmail.com>
2023-06-02 15:17:20 +0400

    add friendshipController

---
+++ src/main/java/com/example/social_media/controller/FriendShipController.java
@@ -0,0 +1,58 @@
+package com.example.social_media.controller;
+
+import com.example.social_media.dto.request.FriendshipRequest;
+import com.example.social_media.dto.request.PostRequest;
+import com.example.social_media.dto.response.FriendshipResponse;
+import com.example.social_media.dto.response.PostResponse;
+import com.example.social_media.service.Imp.FriendshipServiceImp;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * контроллер работы с дружбой пользователей
+ */
+@RestController
+@RequestMapping("/friendship")
+@RequiredArgsConstructor
+public class FriendShipController {
+
+    private final FriendshipServiceImp friendshipService;
+
+    /**
+     * Получение списка дружбы по id пользователя
+     *
+     * @return
+     */
+    @GetMapping("/{id}")
+    public List<FriendshipResponse> getAllFriendshipByUserId(@PathVariable("id")Long id) {
+        return friendshipService.getFriendshipByUserId(id);
+    }
+
+    /**
+     * сохранение новой дружбы
+     * @param friendshipRequest
+     * @return
+     */
+    @PostMapping()
+    public ResponseEntity<HttpStatus> create(@RequestBody FriendshipRequest friendshipRequest) {
+
+        friendshipService.saveFriendship(friendshipRequest);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+    /**
+     * удаление дружбы
+     * @param friendshipRequest
+     * @return
+     */
+    @DeleteMapping()
+    public ResponseEntity<HttpStatus> delete(@RequestBody FriendshipRequest friendshipRequest) {
+
+        friendshipService.deleteFriendship(friendshipRequest);
+        return ResponseEntity.ok(HttpStatus.OK);
+
+    }
+}
---
+++ src/main/java/com/example/social_media/service/FriendshipService.java
@@ -0,0 +1,32 @@
+package com.example.social_media.service;
+
+import com.example.social_media.dto.request.FriendshipRequest;
+import com.example.social_media.dto.response.FriendshipResponse;
+
+import java.util.List;
+
+
+public interface FriendshipService {
+
+    /**
+     * получение дружбы по id пользовавтеля
+     */
+     List<FriendshipResponse> getFriendshipByUserId(Long id);
+
+
+    /**
+     * сохранение новой дружбы
+     *
+     * @param friendshipRequest
+     */
+    void saveFriendship(FriendshipRequest friendshipRequest);
+
+    /**
+     * удаление дружбы
+     *
+     * @param friendshipRequest
+     */
+    void deleteFriendship(FriendshipRequest friendshipRequest);
+
+
+}
